e30961f77f51e1bc5f69123722002609eac7fdcf
Jin Yong <yong@halfhelix.com>
2020-07-13 14:27:11 -0400

    Changes config hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.hosts = (config.hosts rescue []) << 'fenty-seo-prototype.herokuapp.com'
+  config.hosts = (config.hosts rescue []) << 'fsfbseoprototype.com'
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
