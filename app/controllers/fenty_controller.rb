4f9b9f0a79c7fc5ae3e82343a4e46fee76a93f6f
Jin Yong <yong@halfhelix.com>
2020-07-13 16:52:55 -0400

    Fixes git pipeline problem

--- app/controllers/fenty_controller.rb
+++ app/controllers/fenty_controller.rb
@@ -3,6 +3,6 @@
 class FentyController < ApplicationController
   def index
     sitemap_text = 'I think that there should be the content of sitemap.xml here.'
-    render plain: sitemap_text
+    render(plain: sitemap_text)
   end
 end
