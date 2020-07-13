7839fc91270d4cdeceb639f3190637d19d7cc6bd
Jin Yong <yong@halfhelix.com>
2020-07-13 11:24:04 -0400

    Changes database settings for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.hosts = (config.hosts rescue []) << 'fenty-seo-prototype.herokuapp.com'
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
