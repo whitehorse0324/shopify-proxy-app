7c8d787e9d0c95d0798407ef15777cef4b3de5ab
Jin Yong <yong@halfhelix.com>
2020-07-16 16:18:20 -0400

    Adds routes for 2 sitemaps

--- app/controllers/fenty_controller.rb
+++ app/controllers/fenty_controller.rb
@@ -3,7 +3,24 @@
 class FentyController < ApplicationController
   def index
     sitemap_text = '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <!--  This is the parent sitemap linking to additional sitemaps for products, collections and pages as shown below. The sitemap can not be edited manually, but is kept up to date in real time.  -->
+    <sitemap>
+    <loc>https://fsfbseoprototype.com/sitemap_products_1.xml?from=5340960882841&to=5340962979993</loc>
+    </sitemap>
+    <sitemap>
+    <loc>https://fsfbseoprototype.com/sitemap_pages_1.xml</loc>
+    </sitemap>
+    <sitemap>
+    <loc>https://fsfbseoprototype.com/sitemap_collections_1.xml</loc>
+    </sitemap>
+    <sitemap>
+    <loc>https://fsfbseoprototype.com/sitemap_blogs_1.xml</loc>
+    </sitemap>
+    </sitemapindex>'
+    render(plain: sitemap_text)
+  end
+
+  def branded
+    sitemap_text = '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <sitemap>
     <loc>https://branded.fsfbseoprototype.com/sitemap_products_1.xml?from=5340960882841&to=5340962979993</loc>
     </sitemap>
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root :to => 'home#index'
   mount ShopifyApp::Engine, at: '/'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/' => 'home#index'
-  get 'subsitemap' => 'fenty#index'
+  get '/sitemaps/fb-sitemap' => 'fenty#index'
+  get '/sitemaps/fs-sitemap' => 'fenty#branded'
 end
