38c1481f63e7221c5bb37cccf0bb3c05e9c2210d
Jin Yong <yong@halfhelix.com>
2020-07-16 13:59:27 -0400

    Adds sitemap text

--- app/controllers/fenty_controller.rb
+++ app/controllers/fenty_controller.rb
@@ -2,7 +2,21 @@
 
 class FentyController < ApplicationController
   def index
-    sitemap_text = 'I think that there should be the content of sitemap.xml here.'
+    sitemap_text = '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <!--  This is the parent sitemap linking to additional sitemaps for products, collections and pages as shown below. The sitemap can not be edited manually, but is kept up to date in real time.  -->
+    <sitemap>
+    <loc>https://branded.fsfbseoprototype.com/sitemap_products_1.xml?from=5340960882841&to=5340962979993</loc>
+    </sitemap>
+    <sitemap>
+    <loc>https://branded.fsfbseoprototype.com/sitemap_pages_1.xml</loc>
+    </sitemap>
+    <sitemap>
+    <loc>https://branded.fsfbseoprototype.com/sitemap_collections_1.xml</loc>
+    </sitemap>
+    <sitemap>
+    <loc>https://branded.fsfbseoprototype.com/sitemap_blogs_1.xml</loc>
+    </sitemap>
+    </sitemapindex>'
     render(plain: sitemap_text)
   end
 end
