0d56e7e8286d07dffedec4339c5dcfc4bdcc1378
Jin Yong <yong@halfhelix.com>
2020-07-21 17:26:42 -0400

    Changes the way to get urls for 2 domains

--- app/controllers/fenty_controller.rb
+++ app/controllers/fenty_controller.rb
@@ -22,66 +22,116 @@ class FentyController < ApplicationController
     end
     
     products.each do |product|
-      map.add "products/" + product.handle, :updated => product.updated_at, :period => :daily, :priority => 0.5
+      if product.vendor == 'main' and (product.published_scope == 'global' or product.published_scope == 'web') and product.published_at
+        map.add "products/" + product.handle, :updated => product.updated_at, :period => :hourly
+      end
     end
 
     # Get Collections
     cc = ShopifyAPI::CustomCollection.find(:all)
     sc = ShopifyAPI::SmartCollection.find(:all)
     cc.each do |ccol|
-      map.add "collections/" + ccol.handle, :updated => ccol.updated_at, :period => :daily
+      if ccol.published_at
+        map.add "collections/" + ccol.handle, :updated => ccol.updated_at, :period => :hourly
+      end
     end
     sc.each do |scol|
-      map.add "collections/" + scol.handle, :updated => scol.updated_at, :period => :daily
+      if ccol.published_at
+        map.add "collections/" + scol.handle, :updated => scol.updated_at, :period => :hourly
+      end
     end
 
     # Get Pages
     pages = ShopifyAPI::Page.find(:all)
     pages.each do |pageItem|
-      map.add "pages/" + pageItem.handle, :updated => pageItem.updated_at, :period => :weekly
+      if pageItem.published_at
+        map.add "pages/" + pageItem.handle, :updated => pageItem.updated_at, :period => :hourly
+      end
+    end
+
+    # Get blogs
+    blogs = ShopifyAPI::Blog.find(:all)
+    blogs.each do |blogItem|
+      map.add "blogs/" + blogItem.handle, :updated => blogItem.updated_at, :period => :hourly
+    end
+
+    # Get Articles
+    articles = ShopifyAPI::Article.find(:all)
+    articles.each do |articleItem|
+      if articleItem.published_at
+        map.add "articles/" + articleItem.handle, :updated => articleItem.updated_at, :period => :hourly
+      end
     end
     
 
     map.render_to('public/sitemaps/main.xml')
     data = File.read('public/sitemaps/main.xml')
     
-    # ------- Get sitemap.xml content by string -------------
-
-    # data = '<?xml version="1.0" encoding="UTF-8"?>
-    # <urlset xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'
-    
-    # data += "\n<url>"
-    # data += "\n<loc>https://fsfbseoprototype.com/</loc>"
-    # data += "\n<lastmod>2020-07-17T18:28:58Z</lastmod>"
-    # data += "\n<priority>1.0</priority>"
-    # data += "\n</url>"
-    
-    # data += "\n<url>"
-    # data += "\n<loc>https://fsfbseoprototype.com/page1</loc>"
-    # data += "\n<lastmod>2020-07-17T18:28:58Z</lastmod>"
-    # data += "\n</url>"
-    
-    # data += "\n</urlset>"
-
-
     render(plain: data)
   end
 
   def branded
-    sitemap_text = '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <sitemap>
-    <loc>https://branded.fsfbseoprototype.com/sitemap_products_1.xml?from=5340960882841&to=5340962979993</loc>
-    </sitemap>
-    <sitemap>
-    <loc>https://branded.fsfbseoprototype.com/sitemap_pages_1.xml</loc>
-    </sitemap>
-    <sitemap>
-    <loc>https://branded.fsfbseoprototype.com/sitemap_collections_1.xml</loc>
-    </sitemap>
-    <sitemap>
-    <loc>https://branded.fsfbseoprototype.com/sitemap_blogs_1.xml</loc>
-    </sitemap>
-    </sitemapindex>'
-    render(plain: sitemap_text)
+    # Create sitemap object
+    map = XmlSitemap::Map.new('branded.fsfbseoprototype.com', :secure => true)
+    # Connect shopify store by shopify api
+    shop_url = "https://#{ENV['SHOPIFY_API_ACCESS_KEY']}:#{ENV['SHOPIFY_API_ACCESS_PASSWORD']}@fsfbseoprototype.myshopify.com"
+    ShopifyAPI::Base.site = shop_url
+    ShopifyAPI::Base.api_version = ENV['SHOPIFY_API_VERSION']
+    
+    # Get Products
+    products = ShopifyAPI::Product.find(:all, params: {
+      limit: 250
+    })
+    while products.next_page?
+      products = products.fetch_next_page
+      sleep(1.second)
+    end
+    
+    products.each do |product|
+      if product.vendor == 'branded' and (product.published_scope == 'global' or product.published_scope == 'web') and product.published_at
+        map.add "products/" + product.handle, :updated => product.updated_at, :period => :hourly
+      end
+    end
+
+    # Get Collections
+    cc = ShopifyAPI::CustomCollection.find(:all)
+    sc = ShopifyAPI::SmartCollection.find(:all)
+    cc.each do |ccol|
+      if ccol.published_at
+        map.add "collections/" + ccol.handle, :updated => ccol.updated_at, :period => :hourly
+      end
+    end
+    sc.each do |scol|
+      if ccol.published_at
+        map.add "collections/" + scol.handle, :updated => scol.updated_at, :period => :hourly
+      end
+    end
+
+    # Get Pages
+    pages = ShopifyAPI::Page.find(:all)
+    pages.each do |pageItem|
+      if pageItem.published_at
+        map.add "pages/" + pageItem.handle, :updated => pageItem.updated_at, :period => :hourly
+      end
+    end
+
+    # Get blogs
+    blogs = ShopifyAPI::Blog.find(:all)
+    blogs.each do |blogItem|
+      map.add "blogs/" + blogItem.handle, :updated => blogItem.updated_at, :period => :hourly
+    end
+
+    # Get Articles
+    articles = ShopifyAPI::Article.find(:all)
+    articles.each do |articleItem|
+      if articleItem.published_at
+        map.add "articles/" + articleItem.handle, :updated => articleItem.updated_at, :period => :hourly
+      end
+    end
+
+    map.render_to('public/sitemaps/branded.xml')
+    data = File.read('public/sitemaps/branded.xml')
+    
+    render(plain: data)
   end
 end
